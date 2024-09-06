8a436f86fc90469e0382be932d96603b0146de41
Tamimi Ahmad <TamimiGitHub@users.noreply.github.com>
2024-09-06 10:53:06 -0700

    Update stm.rb
    Update homepage url

--- Formula/stm.rb
+++ Formula/stm.rb
@@ -1,15 +1,10 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Stm < Formula
   desc "This is a command line tool to help you get started with Solace PubSub+ Event Broker"
-  homepage "hhttps://github.com/SolaceLabs/solace-tryme-cli"
+  homepage "https://github.com/SolaceLabs/solace-tryme-cli"
   url "https://github.com/SolaceLabs/solace-tryme-cli/releases/download/0.0.50/stm-macos-v0.0.50.zip"
   sha256 "32887729325f59469fdf9d5adf5f0da6e1be6be92cd6ccb3d9685af5521952d2"
   license "Apache-2.0"
-
-  # depends_on "cmake" => :build
-
+  
   def install
     bin.install "stm"
   end
