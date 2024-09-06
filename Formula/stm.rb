5b9b73e4a77aaf3693d1eb64fec7fc19901309bb
Tamimi Ahmad <TamimiGitHub@users.noreply.github.com>
2024-09-06 10:56:22 -0700

    Update stm.rb
    URL typo update

--- Formula/stm.rb
+++ Formula/stm.rb
@@ -1,7 +1,7 @@
 class Stm < Formula
   desc "This is a command line tool to help you get started with Solace PubSub+ Event Broker"
   homepage "https://github.com/SolaceLabs/solace-tryme-cli"
-  url "https://github.com/SolaceLabs/solace-tryme-cli/releases/download/0.0.50/stm-macos-v0.0.50.zip"
+  url "https://github.com/SolaceLabs/solace-tryme-cli/releases/download/v0.0.50/stm-macos-v0.0.50.zip"
   sha256 "32887729325f59469fdf9d5adf5f0da6e1be6be92cd6ccb3d9685af5521952d2"
   license "Apache-2.0"
   
