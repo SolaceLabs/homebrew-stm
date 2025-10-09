6ca6d2ae885c4cbb898b1a493827d46cbd5d5768
SollyBot <community@solace.com>
2025-10-09 10:42:52 +0000

    Update stm to 0.0.83

--- Formula/stm.rb
+++ Formula/stm.rb
@@ -1,8 +1,8 @@
 class Stm < Formula
   desc "This is a command line tool to help you get started with Solace PubSub+ Event Broker"
   homepage "https://github.com/SolaceLabs/solace-tryme-cli"
-  url "https://github.com/SolaceLabs/solace-tryme-cli/releases/download/v0.0.82/stm-macos-v0.0.82.zip"
-  sha256 "6bd15254b8b54259fdf27571b0dfeb7e0dbe88195cc5a7a468be863a7cc67e7a"
+  url "https://github.com/SolaceLabs/solace-tryme-cli/releases/download/v0.0.83/stm-macos-v0.0.83.zip"
+  sha256 "10e0c7537dba38baf42b62692484942765126b27dd4970747507da4c67f26350"
   license "Apache-2.0"
   
   def install
